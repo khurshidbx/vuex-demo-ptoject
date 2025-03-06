daa3c65b70e6ee4fe1bf65dc0cff296e7b250f0c
khurshidbx <jorjbrace@gmail.com>
2025-03-06 11:02:15 +0500

    my first commit

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// const Components = require('unplugin-vue-components/webpack');
+
+// module.exports = {
+//   configureWebpack: {
+//     plugins: [
+//       Components({
+//         dirs: ['src/components'],
+//         extensions: ['vue'],
+//         deep: true,
+//         dts: 'src/components.d.ts',
+//       }),
+//     ],
+//   },
+// };
